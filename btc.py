68d4d0881ea47d2013929f8a2dfee354713675a9
Asim Delalić <asim@mukuru.com>
2019-05-28 14:29:07 +0200

    print columns

--- btc.py
+++ btc.py
@@ -9,12 +9,14 @@ with open('results.csv') as f:
             repo, file, pkey = line.split(",")
             keys.add(pkey)
 
+print "Private Key\t\t\t\t\t\t Public Address\t\t\t\t\t Balance:\t Total In:\t Total Out: \n"
+
 for priv in keys:
     try:
         p = BitcoinPrivateKey(priv)
         pub = p.public_key().address()
         r = requests.get("https://blockchain.info/rawaddr/{}".format(pub))
-        time.sleep(1)
+        time.sleep(2)
         print "{} {} {:20} {:20} {:20}".format(priv, pub, r.json()['final_balance'], r.json()['total_received'], r.json()['total_sent'])
     except (AssertionError, IndexError):
         pass
