8f8d986593e0a3d65eb048ee700fd2dedbf12dab
Asim Delalić <asim@mukuru.com>
2019-05-29 17:14:27 +0200

    small improvements

--- bch.py
+++ bch.py
@@ -3,21 +3,19 @@ import time
 from pybitcoin import BitcoinPrivateKey
 
 keys = set()
-with open('results.csv') as f:
+with open('example.csv') as f:
     for line in f.read().split('\n'):
         if line:
             repo, file, pkey = line.split(",")
             keys.add(pkey)
 
-print "Bch Key\t\t\t\t\t\t Public Address\t\t\t\t\t Balance:\t Total In:\t Total Out: \n"
-
 for priv in keys:
     try:
         p = BitcoinPrivateKey(priv)
         pub = p.public_key().address()
-        r = requests.get("https://rest.bitbox.earth/v1/address/details/{}".format(pub))
-        time.sleep(1)   
-        print "{} {} {:20} {:20} {:20}".format(priv, pub, r.json()['balance'], r.json()['totalReceived'], r.json()['totalSent'])
+        r = requests.get("https://rest.bitcoin.com/v2/address/details/{}".format(pub))
+        print ("Bch Key: {}  Public Address: {}      Balance: {}  Total In: {}    Total Out: {}".format(priv, pub, r.json()['balance'], r.json()['totalReceived'], r.json()['totalSent']))
+        time.sleep(5)   
     except (AssertionError, IndexError):
         pass
     except ValueError:
--- btc.py
+++ btc.py
@@ -3,25 +3,22 @@ import time
 from pybitcoin import BitcoinPrivateKey
 
 keys = set()
-with open('results.csv') as f:
+with open('example.csv') as f:
     for line in f.read().split('\n'):
         if line:
             repo, file, pkey = line.split(",")
             keys.add(pkey)
 
-print "Btc Key\t\t\t\t\t\t Public Address\t\t\t\t\t Balance:\t Total In:\t Total Out: \n"
-
 for priv in keys:
     try:
         p = BitcoinPrivateKey(priv)
         pub = p.public_key().address()
-        r = requests.get("https://blockchain.info/rawaddr/{}".format(pub))
-        time.sleep(2)
-        print "{} {} {:20} {:20} {:20}".format(priv, pub, r.json()['final_balance'], r.json()['total_received'], r.json()['total_sent'])
+        r = requests.get("https://blockchain.info/rawaddr/{}?format=json&limit=100".format(pub))
+        print ("Btc Key: {}  Public Address: {}      Balance: {}  Total In: {}    Total Out: {}".format(priv, pub, r.json()['final_balance'], r.json()['total_received'], r.json()['total_sent']))
+        time.sleep(3)
     except (AssertionError, IndexError):
         pass
     except ValueError:
         print r
         print r.text
-
 
